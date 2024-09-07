491fa78c7fda297dfe66450ccd5b5fbae465cdd8
iulian <zorilaiulian26@gmail.com>
2024-09-07 12:05:50 +0200

    Added files to test the shape retrieval

---
+++ test.py
@@ -0,0 +1,125 @@
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import torch
+import numpy as np
+import os.path as osp
+from utils.eval_utils import nearest_neighbor, show_neighbors, build_gallery, save_load_results
+from model.embedder import Embedder
+from shrec_dataset import ShrecDataset
+import random
+import argparse
+
+def fix_random(seed: int) -> None:
+    """Fix all the possible sources of randomness.
+
+    Args:
+        seed: the seed to use.
+    """
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+def query_model(test_dataset:Dataset, df_test:pd.DataFrame, gallery_shrec:np.array, model:torch.nn.Module, category:str, k:int, device:str):
+  '''
+  Queries the model, by showing the most k similar objects to the given one
+
+  Args:
+      test_dataset: unseen data from which the query will be taken
+      df: dataframe created from test samples
+      gallery_shrec: gallery, i.e. embedded vectors created from the test set
+      model: embedder pytorch model in evaluation state
+      category: category from which the query will be taken
+      k: number of output neighbors to the given query
+      device: CUDA or cpu, on which the query will be evaluated
+  '''
+  fix_random(42)
+  index = df_test.groupby('category').get_group(category).index[0]
+
+  points, label_query = test_dataset[index]
+  original_points = points.copy()
+  points = torch.Tensor(points)
+  points = points.unsqueeze(0).transpose(2, 1).to(device)
+  with torch.no_grad():
+      embedding_points = model(anchor=points)
+      embedding_points = embedding_points.to('cpu').numpy()
+
+  nn_distances, nn_indices = nearest_neighbor(embedding_points,
+                                              index,
+                                              gallery_shrec,
+                                              k)
+
+  classes_map = {v:k for k,v in test_dataset.classes.items()}
+  show_neighbors(index,
+                nn_indices,
+                nn_distances,
+                test_dataset,
+                0.85,
+                classes_map[label_query])
+  
+
+def parse_args():
+    """Parameters"""
+    parser = argparse.ArgumentParser('training')
+    parser.add_argument('--data_root', type=str, help='folder where point cloud data is located')
+    parser.add_argument('--exp_dir', type=str, help='folder from where to load model weights')
+    parser.add_argument('--category', type=str, default="chair", help="category on which shape retrieval will be performed. One of the following: \
+                        ['chair', 'table', 'armchair', 'airplane', 'sword', 'tree', 'cabinet', 'fish', 'bookshelf', 'head', 'laptop', 'guitar', \
+                        'train', 'motorbike', 'cell_phone', 'potted_plant', 'bed', 'helicopter', 'knife', 'keyboard', 'piano', 'couch', 'bus', \
+                        'floor_lamp', 'wheel', 'ship', 'house', 'bicycle', 'face', 'door', 'horse', 'flying_bird', 'hand', 'eyeglasses', 'tablelamp', \
+                        'standing_bird', 'vase', 'spoon', 'flower_with_stem', 'telephone', 'spider', 'race_car', 'violin', 'ant', 'satellite', 'bench', \
+                        'truck', 'car_sedan', 'rifle', 'submarine', 'computer_monitor', 'dolphin'] ")
+    parser.add_argument('--num_neighbors', type=int, default=3, help='number of neighbors to retrieve')
+    parser.add_argument('--batch_size', type=int, default=64, help='batch size in training')
+    parser.add_argument('--num_workers', type=int, default=2, help='number of used workers for dataloader')
+    parser.add_argument('--embeddings_dim', type=int, default=512, help='dimension of the generated embeddings')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    model_weights = "best_model.pth" # @param ["best_model.pth", "model.pth"]
+
+    model_path = f'{args.exp_name}/weights/{model_weights}'
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    embedder = Embedder(size_embedding=args.embeddings_dim, 
+                        normalize_embedding=True, 
+                        train_feature_extactor=True)
+    
+    embedder.to(device)
+
+    #checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
+    print("\n Loading embedder weights \n")
+    checkpoint = torch.load(model_path)
+    embedder.load_state_dict(checkpoint['model_state_dict'])
+
+    test_dataset = ShrecDataset(args.data_root, 'test')
+    sampler = torch.utils.data.SequentialSampler(test_dataset)
+    test_gallery_loader = DataLoader(test_dataset,
+                                    batch_size=args.batch_size,
+                                    shuffle=False,
+                                    pin_memory=True,
+                                    num_workers=args.num_workers,
+                                    sampler=sampler)
+
+    # Create dataframe for query evaluation
+    df_test = pd.DataFrame(test_dataset.samples, columns=['filename','category'])
+
+    embedder = embedder.eval()
+
+    if osp.exists(osp.join(args.exp_name, "gallery_shrec.txt")):
+        recalls_with_triplet, gallery_shrec = save_load_results(args.exp_name, save=False)
+        print(f"\nLoaded gallery from {osp.join(args.exp_name, 'gallery_shrec.txt')} \n")
+    else:
+        gallery_shrec = build_gallery(embedder, test_gallery_loader, device)
+        print(f'The SHREC gallery has: {len(gallery_shrec)} samples')
+    
+    query_model(test_dataset,
+                df_test,
+                gallery_shrec,
+                embedder,
+                args.category,
+                args.num_neighbors)
---
+++ utils/eval_utils.py
@@ -0,0 +1,231 @@
+import torch
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+import plotly.express as px
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors, KDTree
+import open3d as o3d
+import os.path as osp
+import os
+import json
+import shutil
+
+def build_gallery(embedder: torch.nn.Module,
+                  loader: DataLoader,
+                  device: torch.device) -> np.ndarray:
+    """Builds gallery using the images in the dataloader
+
+    Args:
+        embedder: the model to use to get the embedding.
+        loader: the dataloader containing the dataset on which build the gallery.
+        device: the device to use for the model.
+
+    Returns:
+        The gallery, i.e. an array with all the embeddings for the images
+        in the dataset.
+    """
+    gallery = []
+    with torch.no_grad():
+        for points, _ in tqdm(loader):
+            points = torch.Tensor(points).transpose(2, 1)
+            points = points.to(device)
+            batch_embeddings = embedder(points)
+            gallery.extend(batch_embeddings.to('cpu').numpy())
+
+    return np.asarray(gallery)
+
+def visualize_embeddings(ids:np.array, embeddings_reduced:np.array, labels:np.array):
+  '''
+  Visualize reduced embeddings by umap algorithm
+
+  Args:
+      ids: numpy array containing the filenames of the 3d objects
+      embeddings_reduced: reduced embeddings representing the 3d objects through umap
+      labels: category of each 3d object
+  '''
+  df = pd.DataFrame({'id':ids,'x':embeddings_reduced[:,0], 'y':embeddings_reduced[:,1], 'category':labels})
+  fig = px.scatter(df, x="x", y="y", color="category", hover_data=['id'])
+  fig.show()
+
+def nearest_neighbor(sources: np.ndarray,
+                     source_idx: int,
+                     targets: np.ndarray,
+                     num_neighbors: int,
+                     algorithm: str = 'kd_tree') -> tuple[np.ndarray, np.ndarray]:
+    """Computes nearest neighbor search.
+
+    Estimates for each sample in source the nearest neighbor in target using
+    the specified algorithm.
+
+    Args:
+        sources: the source samples.
+        targets: the target samples.
+        num_neighbors: the number of neighbors to find.
+        algorithm: the algorithm to use kd_tree or brute force.
+
+    Returns:
+        The euclidean distance from each sample in source to the nearest neighbor in target.
+        The indices of the nearest neighbor points on target for each sample in source.
+    """
+    kd_tree = NearestNeighbors(n_neighbors=num_neighbors+1,
+                               algorithm=algorithm,
+                               metric='euclidean')
+    kd_tree.fit(targets)
+    distances, indices = kd_tree.kneighbors(sources)
+
+    return distances[indices != source_idx], indices[indices != source_idx]
+
+def read_mesh(path:str) -> o3d.geometry.TriangleMesh:
+  '''Reads triangle mesh and computes its vertex normals.
+
+  Args:
+      path: path from which the mesh is read
+
+  Returns:
+      o3d.geometry.TriangleMesh'''
+  mesh = o3d.io.read_triangle_mesh(path)
+  mesh.compute_vertex_normals()
+  return mesh
+
+def show_neighbors(query_idx,
+                   indices: np.ndarray,
+                   distances: np.ndarray,
+                   dataset: Dataset,
+                   threshold: float,
+                   class_query: str = None) -> None:
+    """Shows the query image together with the found nearest neighbors.
+
+    Args:
+        query_pc: the input point cloud.
+        indices: the indices of nearest neighbors.
+        distances: the distances of nearest neighbors.
+        dataset: the dataset on which the gallery was built.
+        threshold: the matching threshold
+        class_query: the name of the class of the query image.
+
+    Returns:
+    """
+    mesh_dir = 'data/mesh'
+    translation = (1,0,0)
+
+    class_query_title = class_query if class_query else ''
+
+    # Read corresponding mesh format and translate
+    query_mesh_filename = dataset.samples[query_idx][0].replace('.pcd','.off')
+    query_mesh = read_mesh(osp.join(mesh_dir, query_mesh_filename))
+
+    pcd = query_mesh.sample_points_uniformly(number_of_points=1024).translate(translation)
+
+    o3d.visualization.draw_geometries([query_mesh],f'{class_query_title} ({query_mesh_filename})')
+
+    classes_map = {v:k for k,v in dataset.classes.items()}
+
+    for idx, (idx_nn, dist_nn) in enumerate(zip(indices, distances)):
+        color = np.random.rand(3)
+        points, label_matched = dataset[idx_nn]
+        class_match = classes_map[label_matched]
+
+        if class_query is not None:
+            symbol = '✅' if class_query == class_match else '❌'
+        else:
+            symbol = '✅' if dist_nn <= threshold else '❌'
+
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(points)
+
+        # Read corresponding mesh format and translate
+        mesh_filename = dataset.samples[idx_nn][0].replace('.pcd','.off')
+        mesh = read_mesh(osp.join(mesh_dir, mesh_filename))
+        pcd = mesh.sample_points_uniformly(number_of_points=1024).translate(translation)
+
+        o3d.visualization.draw_geometries([mesh], f'{symbol} {class_match} \n Distance: {dist_nn:.3f} ({mesh_filename})')
+
+def get_recalls(model: torch.nn.Module,
+                dataset: Dataset,
+                gallery: np.ndarray,
+                kk: list,
+                exp_name:str,
+                device:str) -> dict:
+    """Computes the recall using different nearest neighbors searches.
+
+    Args:
+        model: the model to use to compute the embedding.
+        dataset: the dataset to use to compute the recall.
+        gallery: the gallery containing all the embeddings for the dataset.
+        kk: the number of nearest neighbors to use for each recall.
+
+    Returns:
+        The computed recalls with different nearest neighbors searches.
+    """
+    model.eval().to(device)
+    max_nn = max(kk)
+    recalls = {idx: 0. for idx in kk}
+    targets = np.asarray(dataset.targets)
+    tree = KDTree(gallery)
+
+    # Create a numpy array of datapaths (e.g. tree/M001285.pcd)
+    datapaths = np.array(list(map(lambda x: x[0], dataset.samples)))
+    root = osp.join('drive/MyDrive/ShapeRetrieval/results', exp_name, 'inference')
+    if not osp.exists(root):
+      os.mkdir(root)
+    else:
+      shutil.rmtree(root)
+
+    for i, (points, label_query) in enumerate(tqdm(dataset)):
+        points = torch.Tensor(points).unsqueeze(0).to(device)
+        points = points.transpose(2, 1)
+
+        category = dataset.samples[i][1]
+        if not osp.exists(osp.join(root, category)): os.mkdir(osp.join(root, category))
+
+        with torch.no_grad():
+            embedding = model(anchor=points)
+            embedding = embedding.to('cpu').numpy()
+
+            _, indices_matched = tree.query(embedding, k=max_nn + 1)
+            indices_matched = indices_matched[0]
+
+            # Save matched neighbours for a single sample in a .txt file
+            filename = osp.basename(datapaths[i]).replace('.pcd','.txt')
+            filepath = osp.join(root, category, filename)
+            np.savetxt(filepath, datapaths[indices_matched], fmt='%s')
+
+            for k in kk:
+                indices_matched_temp = indices_matched[1:k + 1]
+                classes_matched = targets[indices_matched_temp]
+                recalls[k] += np.count_nonzero(classes_matched == label_query) > 0
+
+    for key, value in recalls.items():
+        recalls[key] = value / (1.0 * len(dataset))
+
+    print(f"\nSaved inference results in {root}")
+
+    return recalls
+
+def save_load_results(directory:str, data:tuple=None, save:bool=True) -> tuple:
+  '''
+  Saves or loads results depending on the 'save' variable
+
+  Args:
+      directory: root directory in which to save/load results
+      data: contains recall scores dictionary and the gallery
+      save: bool which specifies whether data should be saved or loaded
+  Returns:
+      recall scores dictionary and gallery when 'save' is False
+  '''
+  recalls_filename = "point_tnt_recall.json"
+  gallery_filename = "gallery_shrec.txt"
+
+  if save:
+    if data is not None:
+      recalls_with_triplet, gallery_shrec = data
+      with open(osp.join(directory, recalls_filename), "w") as f:
+        json.dump(recalls_with_triplet, f)
+      np.savetxt(osp.join(directory, gallery_filename), gallery_shrec, fmt='%f')
+      return None
+  else:
+    with open(osp.join(directory, recalls_filename)) as f:
+      recalls_with_triplet = json.load(f)
+    gallery_shrec = np.loadtxt(osp.join(directory, gallery_filename), dtype=float)
+    return recalls_with_triplet, gallery_shrec
